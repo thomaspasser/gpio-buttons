8aec2a3ccb499576d589e000e7f01ccc225ce863
tomatpasser <tomatpasser@gmail.com>
2016-07-17 14:41:12 +0200

    Fix for permissions, if not included in build

--- app/plugins/miscellanea/gpio-buttons/install.sh
+++ app/plugins/miscellanea/gpio-buttons/install.sh
@@ -4,4 +4,15 @@ echo "Initializing config"
 mkdir /data/configuration/miscellanea/gpio-buttons
 touch /data/configuration/miscellanea/gpio-buttons/config.json
 
+if [ ! -f "/lib/udev/rules.d/91-gpio.rules" ];
+	then
+		echo "GPIO permission rules doesn't exist, creating"
+		cp /data/plugins/miscellanea/gpio-buttons/fix.tar.gz /
+		cd /
+		sudo tar -xvf fix.tar.gz
+		rm /fix.tar.gz
+	else
+		echo "GPIO permission rules already exists"
+fi
+
 echo "plugininstallend"
