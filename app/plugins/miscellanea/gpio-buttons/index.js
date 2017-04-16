f5ecff193317296fa6d4b035004f0d11f0f8c34b
macmpi <spam@ipik.org>
2017-04-16 23:16:15 +0200

    index.js reformatting
    ended-up reworking few things: is not that radical as diff may look though... (better to look at final file)
    - renamed the actions (easier to read), including in config.json and UIConfig for consistency
    - removed getActionName: not really needed: log message works nicely without it
    - refactored applyConf (and removed setConf) into createTriggers, saveConfig (for consistency naming)
    - changed some "for <array>" into "<array>.forEach"
    - cleaned-up some (unused) prototype functions in case of future need
    - ...and simplified onStart & onStop promises (and removed elsewhere as all is supposed to be synchronous for now)

--- app/plugins/miscellanea/gpio-buttons/index.js
+++ app/plugins/miscellanea/gpio-buttons/index.js
@@ -5,7 +5,7 @@ var fs = require('fs-extra');
 var Gpio = require('onoff').Gpio;
 var io = require('socket.io-client');
 var socket = io.connect('http://localhost:3000');
-var actions = ["playpause", "volup", "voldown", "previous", "next", "shutdown"];
+var actions = ["Play_Pause", "Volume_up", "Volume_down", "Previous", "Next", "Shutdown"];
 
 module.exports = GPIOButtons;
 
@@ -14,7 +14,6 @@ function GPIOButtons(context) {
 	self.context=context;
 	self.commandRouter = self.context.coreCommand;
 	self.logger = self.context.logger;
-
 	self.triggers = [];
 }
 
@@ -27,31 +26,24 @@ GPIOButtons.prototype.onVolumioStart = function () {
 	this.config.loadFile(configFile);
 
 	self.logger.info("GPIO-Buttons initialized");
-
-}
+};
 
 
 GPIOButtons.prototype.getConfigurationFiles = function()
 {
 	return ['config.json'];
-}
-
-
+};
 
 
 GPIOButtons.prototype.onStart = function () {
 	var self = this;
 
-	var defer=libQ.defer();
-
-	self.applyConf();
+	self.createTriggers();
 	self.logger.info("GPIO-Buttons started");
-
-	defer.resolve();
 	
-	return defer.promise;
-
+    return libQ.resolve();
 };
+
 
 GPIOButtons.prototype.onStop = function () {
 	var self = this;
@@ -59,28 +51,42 @@ GPIOButtons.prototype.onStop = function () {
 	self.clearTriggers();
 	self.logger.info("GPIO-Buttons stopped");
 
-	return libQ.resolve();
+    return libQ.resolve();
 };
+
 
 GPIOButtons.prototype.onRestart = function () {
 	var self = this;
-
 };
 
 GPIOButtons.prototype.onInstall = function () {
 	var self = this;
-
 };
 
 GPIOButtons.prototype.onUninstall = function () {
 	var self = this;
-
 };
 
-GPIOButtons.prototype.getConf = function () {
+GPIOButtons.prototype.getConf = function (varName) {
 	var self = this;
-
 };
+
+GPIOButtons.prototype.setConf = function(varName, varValue) {
+	var self = this;
+};
+
+GPIOButtons.prototype.getAdditionalConf = function (type, controller, data) {
+	var self = this;
+};
+
+GPIOButtons.prototype.setAdditionalConf = function () {
+	var self = this;
+};
+
+GPIOButtons.prototype.setUIConfig = function (data) {
+	var self = this;
+};
+
 
 GPIOButtons.prototype.getUIConfig = function () {
 	var defer = libQ.defer();
@@ -99,118 +105,41 @@ GPIOButtons.prototype.getUIConfig = function () {
         .then(function(uiconf)
         {
 
-		var i = 0;
-		for(var j in actions){
-			var action = actions[j]
+			var i = 0;
+			actions.forEach(function(action, index, array) {
+ 				
+ 				// Strings for config
+				var c1 = action.concat('.enabled');
+				var c2 = action.concat('.pin');
+				
+				// accessor supposes actions and uiconfig items are in SAME order
+				// this is potentially dangerous: rewrite with a JSON search of "id" value ?				
+				uiconf.sections[0].content[2*i].value = self.config.get(c1);
+				uiconf.sections[0].content[2*i+1].value.value = self.config.get(c2);
+				uiconf.sections[0].content[2*i+1].value.label = self.config.get(c2).toString();
 
-			// Strings for config
-			var c1 = action.concat('.enabled');
-			var c2 = action.concat('.pin');
-			uiconf.sections[0].content[2*i].value = self.config.get(c1);
-			uiconf.sections[0].content[2*i+1].value.value = self.config.get(c2);
-			uiconf.sections[0].content[2*i+1].value.label = self.config.get(c2).toString();
-			i = i + 1;
-		}
+				i = i + 1;
+			});
 
             defer.resolve(uiconf);
-					})
-                .fail(function()
-            {
-                defer.reject(new Error());
+		})
+        .fail(function()
+        {
+            defer.reject(new Error());
         });
 
         return defer.promise;
 };
 
-GPIOButtons.prototype.setUIConfig = function (data) {
-	var self = this;
 
-	var uiconf=fs.readJsonSync(__dirname+'/UIConfig.json');
-};
-
-
-
-GPIOButtons.prototype.clearTriggers = function () {
-	var self = this;
-	var defer=libQ.defer();
-	
-	for (var i in self.triggers) {
-		var trigger = self.triggers[i];
-		self.logger.info("GPIO-Buttons: Destroying trigger " + i);
-
-		trigger.unwatchAll();
-		trigger.unexport();
-	}
-	self.triggers = [];
-	
-	return defer.promise;
-};
-
-GPIOButtons.prototype.setConf = function () {
-	var self = this;
-
-	self.clearTriggers();
-	self.applyConf();
-
-};
-
-GPIOButtons.prototype.getSystemConf = function (pluginName, varName) {
-	var self = this;
-
-};
-
-GPIOButtons.prototype.setSystemConf = function (pluginName, varName) {
-	var self = this;
-
-};
-
-GPIOButtons.prototype.getAdditionalConf = function () {
-	var self = this;
-
-};
-
-GPIOButtons.prototype.setAdditionalConf = function () {
-	var self = this;
-
-};
-
-GPIOButtons.prototype.applyConf = function() {
-	var self = this;
-
-	self.logger.info('GPIO-Buttons: Applying config file...');
-
-
-	for(var i in actions){
-		var action = actions[i];
-		// Strings for config
-		var c1 = action.concat('.enabled');
-		var c2 = action.concat('.pin');
-
-		var enabled = self.config.get(c1);
-		var pin = self.config.get(c2);
-
-		if(enabled === true){
-			self.logger.info('GPIO-Buttons: '+ self.getActionName(action) + ' on pin ' + pin);
-			var j = new Gpio(pin,'in','both');
-			j.watch(self.listener.bind(self,action));
-			self.triggers.push(j);
-		}
-	}
-
-}
-
-GPIOButtons.prototype.saveTriggers=function(data)
+GPIOButtons.prototype.saveConfig = function(data)
 {
 	var self = this;
 
-	var defer = libQ.defer();
-
-	for(var i in actions){
-		var action = actions[i];
-
-		// Strings for data fields
-		var s1 = action.concat('enabled');
-		var s2 = action.concat('pin');
+	actions.forEach(function(action, index, array) {
+ 		// Strings for data fields
+		var s1 = action.concat('_enabled');
+		var s2 = action.concat('_pin');
 
 		// Strings for config
 		var c1 = action.concat('.enabled');
@@ -220,42 +149,49 @@ GPIOButtons.prototype.saveTriggers=function(data)
 		self.config.set(c1, data[s1]);
 		self.config.set(c2, data[s2]['value']);
 		self.config.set(c3, 0);
+	});
 
-	}
-
-	self.setConf();
+	self.clearTriggers();
+	self.createTriggers();
 
 	self.commandRouter.pushToastMessage('success',"GPIO-Buttons", "Configuration saved");
+};
 
-	defer.resolve({});
-	return defer.promise;
-}
 
-GPIOButtons.prototype.getActionName = function(action) {
-	var actionName;
+GPIOButtons.prototype.createTriggers = function() {
+	var self = this;
 
-	switch(action){
-		case 'playpause':
-			actionName = "Play/pause";
-			break;
-		case 'next':
-			actionName = "Next";
-			break;
-		case 'previous':
-			actionName = "Previous";
-			break;
-		case 'volup':
-			actionName = "Vol+";
-			break;
-		case 'voldown':
-			actionName = "Vol-";
-			break;
-		case 'shutdown':
-			actionName = "Shutdown";
-			break;
-	}
-	return actionName;
-}
+	self.logger.info('GPIO-Buttons: Reading config and creating triggers...');
+
+	actions.forEach(function(action, index, array) {
+		var c1 = action.concat('.enabled');
+		var c2 = action.concat('.pin');
+
+		var enabled = self.config.get(c1);
+		var pin = self.config.get(c2);
+
+		if(enabled === true){
+			self.logger.info('GPIO-Buttons: '+ action + ' on pin ' + pin);
+			var j = new Gpio(pin,'in','both');
+			j.watch(self.listener.bind(self,action));
+			self.triggers.push(j);
+		}
+	});
+};
+
+
+GPIOButtons.prototype.clearTriggers = function () {
+	var self = this;
+	
+	self.triggers.forEach(function(trigger, index, array) {
+  		self.logger.info("GPIO-Buttons: Destroying trigger " + index);
+
+		trigger.unwatchAll();
+		trigger.unexport();		
+	});
+	self.triggers = [];	
+};
+
 
 GPIOButtons.prototype.listener = function(action,err,value){
 	var self = this;
@@ -270,10 +206,14 @@ GPIOButtons.prototype.listener = function(action,err,value){
 	}
 	// remember value
 	self.config.set(c3,value);
+};
 
-}
 
-GPIOButtons.prototype.playpause = function() {
+
+
+
+//Play / Pause
+GPIOButtons.prototype.Play_Pause = function() {
   //this.logger.info('GPIO-Buttons: Play/pause button pressed');
   socket.emit('getState','');
   socket.once('pushState', function (state) {
@@ -285,34 +225,34 @@ GPIOButtons.prototype.playpause = function() {
       socket.emit('play');
     }
   });
-}
+};
 
 //Next on playlist
-GPIOButtons.prototype.next = function() {
+GPIOButtons.prototype.Next = function() {
   //this.logger.info('GPIO-Buttons: Next-button pressed');
   socket.emit('next')
-}
+};
 
 //Previous on playlist
-GPIOButtons.prototype.previous = function() {
+GPIOButtons.prototype.Previous = function() {
   //this.logger.info('GPIO-Buttons: Previous-button pressed');
   socket.emit('prev')
-}
+};
 
 //Volume up
-GPIOButtons.prototype.volup = function() {
+GPIOButtons.prototype.Volume_up = function() {
   //this.logger.info('GPIO-Buttons: Vol+ button pressed');
   socket.emit('volume','+');
-}
+};
 
 //Volume down
-GPIOButtons.prototype.voldown = function() {
+GPIOButtons.prototype.Volume_down = function() {
   //this.logger.info('GPIO-Buttons: Vol- button pressed\n');
   socket.emit('volume','-');
-}
+};
 
 //Shutdown
-GPIOButtons.prototype.shutdown = function() {
-  //this.logger.info('GPIO-Buttons: Shutdown button pressed\n');
+GPIOButtons.prototype.Shutdown = function() {
+  // this.logger.info('GPIO-Buttons: Shutdown button pressed\n');
   this.commandRouter.shutdown();
-}
+};
