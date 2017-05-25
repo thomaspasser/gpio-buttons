f1420d857d380f477dfbe9778e75810a8452d56f
tomatpasser <tomatpasser@gmail.com>
2017-05-25 21:24:43 +0200

    Update install script for backwards compatibility of config file

--- app/plugins/system_controller/gpio-buttons/install.sh
+++ app/plugins/system_controller/gpio-buttons/install.sh
@@ -2,11 +2,12 @@
 
 echo "Initializing config"
 
-# compatibilty with earlier config files due to few commands name change
+# compatibilty with earlier config files due to few commands name change and directory rename
 if [ -f /data/configuration/miscellanea/gpio-buttons/config.json ];then
-	sed -i 's/playpause/playPause/g' /data/configuration/miscellanea/gpio-buttons/config.json
-	sed -i 's/volup/volumeUp/g' /data/configuration/miscellanea/gpio-buttons/config.json
-	sed -i 's/voldown/volumeDown/g' /data/configuration/miscellanea/gpio-buttons/config.json
+	mv /data/configuration/miscellanea/gpio-buttons/config.json /data/configuration/system_controller/gpio-buttons/config.json
+	sed -i 's/playpause/playPause/g' /data/configuration/system_controller/gpio-buttons/config.json
+	sed -i 's/volup/volumeUp/g' /data/configuration/system_controller/gpio-buttons/config.json
+	sed -i 's/voldown/volumeDown/g' /data/configuration/system_controller/gpio-buttons/config.json
 fi
 
 echo "plugininstallend"
